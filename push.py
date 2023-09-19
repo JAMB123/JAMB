da29361a80688dbcf0df36052488fa82feb8325f
JAMB123 <jamblpa476@gmail.com>
2023-09-19 15:37:44 -0300

    Favoritar

--- push.py
+++ push.py
@@ -7,3 +7,5 @@ system('git config user.email "jamblpa476@gmail.com"')
 system('git add .')
 system('git commit -m "' + message + '"')
 system('git push origin master')
+
+#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
